6c931a85db548300c17243a295e6a92df2c35662
indushasagi <indusha.sagi@gmail.com>
2019-08-20 14:56:49 +0530

    test

--- src/index.js
+++ src/index.js
@@ -4,14 +4,13 @@ import App from "./components/App";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./reducers";
-import { addCharacterById } from "./actions";
+
 
 import "./styles.css";
 
 const store = createStore(rootReducer);
 console.log("store.getState()", store.getState());
-store.subscribe(() => console.log("store", store.getState()));
-store.dispatch(addCharacterById(2));
+
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
